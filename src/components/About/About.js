85823f1950b202880db1e8329942c7292f914728
DevBGarcia <DevBGarcia@gmail.com>
2024-10-06 17:23:52 -0700

    checkpoint

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,54 +1,53 @@
-import GitHubIcon from '@material-ui/icons/GitHub'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import { about } from '../../portfolio'
 import './About.css'
 
 const About = () => {
-  const { name, role, description, resume, social } = about
+  const { name, role, resume, social } = about
 
   return (
     <div className='about center'>
       {name && (
         <h1>
-          Hi, I am <span className='about__name'>{name}.</span>
+          <span className='about__name'>Byron Garcia</span>
         </h1>
       )}
 
-      {role && <h2 className='about__role'>A {role}.</h2>}
-      <p className='about__desc'>{description && description}</p>
+      {role && <h2 className='about__role'>Software Engineer.</h2>}
+      <p className='about__desc'>
+        I am a dedicated engineer who thrives on contributing to a healthy and
+        spositive environment. As a team player, I am committed to going above
+        and beyond to support my colleagues and ensure the success of the
+        company. I am a valuable asset to any team and continuously strive
+        toenhance my engineering skills and knowledge.
+      </p>
 
       <div className='about__contact center'>
-        {resume && (
-          <a href={resume}>
-            <span type='button' className='btn btn--outline'>
-              Resume
-            </span>
-          </a>
-        )}
-
-        {social && (
-          <>
-            {social.github && (
-              <a
-                href={social.github}
-                aria-label='github'
-                className='link link--icon'
-              >
-                <GitHubIcon />
-              </a>
-            )}
-
-            {social.linkedin && (
-              <a
-                href={social.linkedin}
-                aria-label='linkedin'
-                className='link link--icon'
-              >
-                <LinkedInIcon />
-              </a>
-            )}
-          </>
-        )}
+        <a
+          href='https://github.com/DevBGarcia'
+          aria-label='github'
+          className='link link--icon'
+        >
+          <GitHubIcon />
+          Github
+        </a>
+        <a
+          href='https://github.com/DevBGarcia'
+          aria-label='github'
+          className='link link--icon'
+        >
+          <GitHubIcon />
+          Github
+        </a>
+        <a
+          href='https://www.linkedin.com/in/byron-garcia-a25987138/'
+          aria-label='linkedin'
+          className='link link--icon'
+        >
+          <LinkedInIcon />
+          Linked In
+        </a>
       </div>
     </div>
   )
