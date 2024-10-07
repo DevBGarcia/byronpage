0ace3eb6855215c2eb6a19d623136b0826a3f8fd
DevBGarcia <DevBGarcia@gmail.com>
2024-10-07 16:39:15 -0700

    update to not use dark theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import { useContext } from 'react'
-import { ThemeContext } from './contexts/theme'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
 import Skills from './components/Skills/Skills'
 import Contact from './components/Contact/Contact'
 import './App.css'
 
-const App = () => {
-  const [{ themeName }] = useContext(ThemeContext)
-
-  return (
-    <div id='top' className={`${themeName} app`}>
-      <main>
-        <About />
-        <Projects />
-        <Skills />
-        <Contact />
-      </main>
-    </div>
-  )
-}
+const App = () => (
+  <div id='top' className='light app'>
+    <main>
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </main>
+  </div>
+)
 
 export default App
