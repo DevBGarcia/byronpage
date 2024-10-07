5c67919a7ad332ea0fe0b855b999dc1e3c5d0624
DevBGarcia <DevBGarcia@gmail.com>
2024-10-06 17:27:45 -0700

    undo package changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,22 +1,51 @@
 import uniqid from 'uniqid'
-import { projects } from '../../portfolio'
 import ProjectContainer from '../ProjectContainer/ProjectContainer'
 import './Projects.css'
 
-const Projects = () => {
-  if (!projects.length) return null
+const Projects = () => (
+  <section id='projects' className='section projects'>
+    <h2 className='section__title'>Personal Project</h2>
 
-  return (
-    <section id='projects' className='section projects'>
-      <h2 className='section__title'>Projects</h2>
+    <div className='projects__grid'>
+      <div className='project'>
+        <h3>Spartan HIIT Timer (Android)</h3>
 
-      <div className='projects__grid'>
-        {projects.map((project) => (
-          <ProjectContainer key={uniqid()} project={project} />
-        ))}
+        <p className='project__description'>
+          Simple mobile timer app built with react-native, meant to be used for
+          interval based exercises. The app is currently in internal beta
+          testing and under review by the android play store but please feel to
+          check out the github repot or download the APK directly.
+        </p>
+        <ul className='project__stack'>
+          {['TypeScript', 'React-native'].map((item) => (
+            <li key={uniqid()} className='project__stack-item'>
+              {item}
+            </li>
+          ))}
+        </ul>
+
+        {/* {project.sourceCode && (
+          <a
+            href={project.sourceCode}
+            aria-label='source code'
+            className='link link--icon'
+          >
+            <GitHubIcon />
+          </a>
+        )} */}
+
+        {/* {project.livePreview && (
+          <a
+            href={project.livePreview}
+            aria-label='live preview'
+            className='link link--icon'
+          >
+            <LaunchIcon />
+          </a>
+        )} */}
       </div>
-    </section>
-  )
-}
+    </div>
+  </section>
+)
 
 export default Projects
